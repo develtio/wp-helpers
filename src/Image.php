6e19a677eb07e83dc6cc4c1ca1c8e1fdcc39845f
Tomasz Rusiecki <rusiecki.tom@gmail.com>
2020-10-27 23:08:45 +0100

    parseImage

--- src/Image.php
+++ src/Image.php
@@ -14,10 +14,25 @@ final class Image
     }
 
     /**
+     * @param $image
+     * @param string $size
+     * @return array|null
+     */
+    public static function parseImage($image, $size = 'thumbnail')
+    {
+        if (!self::isValid($image)) return null;
+
+        return [
+            'alt' => $image['alt'],
+            'url' => $image['sizes'][$size]
+        ];
+    }
+
+    /**
      * @param int $id
      * @return string
      */
-    public static function getAlt(int $id): string
+    public static function getAttachmentAlt(int $id): string
     {
         return get_post_meta($id, '_wp_attachment_image_alt', true);
     }
